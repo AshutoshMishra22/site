ab197eb0dbff8681a6ccdbdd0c4c037651cf9c7d
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-01-08 22:45:33 +0530

    fixing caching

--- public/sw.js
+++ public/sw.js
@@ -11,6 +11,9 @@ var url_Cache = [
   "/static/media",
   "/favicon.ico",
   "/images/icons/icon-192x192.png",
+  "/site/manifest.json",
+  "site/static/css/main.d95d4030.css",
+  "site/static/js/main.06889904.js",
 ];
 
 //install

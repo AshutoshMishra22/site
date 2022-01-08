c8639ade0837af29f5cb02e13880e3bb7fb5a79e
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-01-08 15:50:17 +0530

    Static UI Complete

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-const Article = () => {
-  return <div>Article</div>;
+import "./article.css";
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className='gpt3_blog-container_article'>
+      <div className='gpt3_blog-continer_article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3_blog-continer_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className='gpt3_cta'>
+      <div className='gpt3_cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3_cta-btn'>
+        <button type='button' title='Get Started'>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;

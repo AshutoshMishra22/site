057041ca24ee4d0c69aacf1fbac7b1e903d61db9
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-01-08 17:12:04 +0530

    server worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorkerRegistration.register();
